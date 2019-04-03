f0b24c950bda9793fc969a7145394f68b2bf4667
happyguys8 <aaapppro@gmail.com>
2019-04-04 00:38:00 +0800

    modification

--- Nutrition Project/nutrition/lib/main.dart
+++ Nutrition Project/nutrition/lib/main.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'dart:convert';
 
-class Spacecraft {
-  final String id;
-  final String seq, name, spec, imageUrl;
+class Organ {
+  final String id, seq, name, spec, imageUrl;
 
-  Spacecraft({
+  Organ({
     this.id,
     this.seq,
     this.name,
@@ -16,49 +15,71 @@ class Spacecraft {
     this.imageUrl,
   });
 
-  factory Spacecraft.fromJson(Map<String, dynamic> jsonData) {
-    return Spacecraft(
+  factory Organ.fromJson(Map<String, dynamic> jsonData) {
+    return Organ(
       id: jsonData['id'],
       seq: jsonData['seq'],
       name: jsonData['name'],
       spec: jsonData['spec'],
-      imageUrl: "http://192.168.12.2/PHP/spacecrafts/images/"+jsonData['image_url'],
+      imageUrl: "http://www.groupbean.com/images/"+jsonData['image_url'],
     );
   }
 }
 
-class CustomListView extends StatelessWidget {
-  final List<Spacecraft> spacecrafts;
+class OrganItem {
+  final String id, seq, name, spec, imageUrl;
 
-  CustomListView(this.spacecrafts);
+  OrganItem({
+    this.id,
+    this.seq,
+    this.name,
+    this.spec,
+    this.imageUrl,
+  });
+
+  factory OrganItem.fromJson(Map<String, dynamic> jsonData) {
+    return OrganItem(
+      id: jsonData['id'],
+      seq: jsonData['seq'],
+      name: jsonData['name'],
+      spec: jsonData['spec'],
+      imageUrl: "http://www.groupbean.com/images/"+jsonData['image_url'],
+    );
+  }
+}
+
+class OrganListView extends StatelessWidget {
+  final List<Organ> organs;
+
+  OrganListView(this.organs);
 
   Widget build(context) {
     return ListView.builder(
-      itemCount: spacecrafts.length,
+      itemCount: organs.length,
       itemBuilder: (context, int currentIndex) {
-        return createViewItem(spacecrafts[currentIndex], context);
+        return createViewItem(organs[currentIndex], context);
       },
     );
   }
 
-  Widget createViewItem(Spacecraft spacecraft, BuildContext context) {
+  Widget createViewItem(Organ organ, BuildContext context) {
     return new ListTile(
         title: new Card(
           elevation: 5.0,
           child: new Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(20.0),
+            //decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.all(10.0),
             child: Column(
               children: <Widget>[
                 Padding(
-                  child: Image.network(spacecraft.imageUrl),
+                  child: Image.network(organ.imageUrl),
                   padding: EdgeInsets.only(bottom: 8.0),
                 ),
                 Row(children: <Widget>[
                   Padding(
                       child: Text(
-                        spacecraft.name,
+                        organ.name,
                         style: new TextStyle(fontWeight: FontWeight.bold),
                         textAlign: TextAlign.right,
                       ),
@@ -66,7 +87,7 @@ class CustomListView extends StatelessWidget {
                   Text(" | "),
                   Padding(
                       child: Text(
-                        spacecraft.spec,
+                        organ.spec,
                         style: new TextStyle(fontStyle: FontStyle.italic),
                         textAlign: TextAlign.right,
                       ),
@@ -82,7 +103,71 @@ class CustomListView extends StatelessWidget {
           //screen with a platform-adaptive transition.
           var route = new MaterialPageRoute(
             builder: (BuildContext context) =>
-            new SecondScreen(value: spacecraft),
+            new SecondScreen(value: organ),
+          );
+          //A Navigator is a widget that manages a set of child widgets with
+          //stack discipline.It allows us navigate pages.
+          Navigator.of(context).push(route);
+        });
+  }
+}
+
+class OrganItemListView extends StatelessWidget {
+  final List<OrganItem> organItems;
+
+  OrganItemListView(this.organItems);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: organItems.length,
+      itemBuilder: (context, int currentIndex) {
+        return createViewItem(organItems[currentIndex], context);
+      },
+    );
+  }
+
+  Widget createViewItem(OrganItem organItem, BuildContext context) {
+    return new ListTile(
+        title: new Card(
+          elevation: 5.0,
+          child: new Container(
+            //decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  child: Image.network(organItem.imageUrl),
+                  padding: EdgeInsets.only(bottom: 8.0),
+                ),
+                Row(children: <Widget>[
+                  Padding(
+                      child: Text(
+                        organItem.name,
+                        style: new TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.all(1.0)),
+                  Text(" | "),
+                  Padding(
+                      child: Text(
+                        organItem.spec,
+                        style: new TextStyle(fontStyle: FontStyle.italic),
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.all(1.0)),
+                ]),
+              ],
+            ),
+          ),
+        ),
+        onTap: () {
+          //We start by creating a Page Route.
+          //A MaterialPageRoute is a modal route that replaces the entire
+          //screen with a platform-adaptive transition.
+          var route = new MaterialPageRoute(
+            builder: (BuildContext context) =>
+            new ThirdScreen(value: organItem),
           );
           //A Navigator is a widget that manages a set of child widgets with
           //stack discipline.It allows us navigate pages.
@@ -92,23 +177,40 @@ class CustomListView extends StatelessWidget {
 }
 
 //Future is n object representing a delayed computation.
-Future<List<Spacecraft>> downloadJSON() async {
+Future<List<Organ>> downloadJSON() async {
   final jsonEndpoint =
       "http://www.groupbean.com/api/DiplayOrgans.php?LngTyp=1";
 
   final response = await get(jsonEndpoint);
 
   if (response.statusCode == 200) {
-    List spacecrafts = json.decode(response.body);
-    return spacecrafts
-        .map((spacecraft) => new Spacecraft.fromJson(spacecraft))
+    List organs = json.decode(response.body);
+    return organs
+        .map((organ) => new Organ.fromJson(organ))
         .toList();
   } else
     throw Exception('We were not able to successfully download the json data.');
 }
 
+//Future is n object representing a delayed computation.
+Future<List<OrganItem>> downloadItemJSON() async {
+  final jsonEndpoint =
+      "http://www.groupbean.com/api/DisplayOrgansItem.php?LngTyp=1";
+
+  final response = await get(jsonEndpoint);
+
+  if (response.statusCode == 200) {
+    List organItems = json.decode(response.body);
+    return organItems
+        .map((organItem) => new OrganItem.fromJson(organItem))
+        .toList();
+  } else
+    throw Exception('We were not able to successfully download the json data.');
+}
+
+
 class SecondScreen extends StatefulWidget {
-  final Spacecraft value;
+  final Organ value;
 
   SecondScreen({Key key, this.value}) : super(key: key);
 
@@ -116,7 +218,49 @@ class SecondScreen extends StatefulWidget {
   _SecondScreenState createState() => _SecondScreenState();
 }
 
+class ThirdScreen extends StatefulWidget {
+  final OrganItem value;
+
+  ThirdScreen({Key key, this.value}) : super(key: key);
+
+  @override
+  _ThirdScreenState createState() => _ThirdScreenState();
+}
+
+
 class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Detail Page')),
+      body: new Container(
+
+          child: new FutureBuilder<List<OrganItem>>(
+            future: downloadItemJSON(),
+            //we pass a BuildContext and an AsyncSnapshot object which is an
+            //Immutable representation of the most recent interaction with
+            //an asynchronous computation.
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<OrganItem> organs = snapshot.data;
+                return new OrganItemListView(organs);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              //return  a circular progress indicator.
+              return new CircularProgressIndicator();
+            },
+          )
+
+
+
+      ),
+      
+    );
+  }
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -133,17 +277,15 @@ class _SecondScreenState extends State<SecondScreen> {
                 ),
                 padding: EdgeInsets.only(bottom: 20.0),
               ),
-              /*
               Padding(
                 //`widget` is the current configuration. A State object's configuration
                 //is the corresponding StatefulWidget instance.
-                child: Image.network('${widget.value.imageUrl}'),
+                child: Image.network("http://www.groupbean.com/images/"+ "${widget.value.imageUrl}"),
                 padding: EdgeInsets.all(12.0),
               ),
-              */
               Padding(
                 child: new Text(
-                  'NAME : ${widget.value.name}',
+                  'Name : ${widget.value.name}',
                   style: new TextStyle(fontWeight: FontWeight.bold),
                   textAlign: TextAlign.left,
                 ),
@@ -151,7 +293,7 @@ class _SecondScreenState extends State<SecondScreen> {
               ),
               Padding(
                 child: new Text(
-                  'PROPELLANT : ${widget.value.spec}',
+                  'Spec : ${widget.value.spec}',
                   style: new TextStyle(fontWeight: FontWeight.bold),
                   textAlign: TextAlign.left,
                 ),
@@ -163,6 +305,8 @@ class _SecondScreenState extends State<SecondScreen> {
     );
   }
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -176,15 +320,15 @@ class MyApp extends StatelessWidget {
         body: new Center(
           //FutureBuilder is a widget that builds itself based on the latest snapshot
           // of interaction with a Future.
-          child: new FutureBuilder<List<Spacecraft>>(
+          child: new FutureBuilder<List<Organ>>(
             future: downloadJSON(),
             //we pass a BuildContext and an AsyncSnapshot object which is an
             //Immutable representation of the most recent interaction with
             //an asynchronous computation.
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                List<Spacecraft> spacecrafts = snapshot.data;
-                return new CustomListView(spacecrafts);
+                List<Organ> organs = snapshot.data;
+                return new OrganListView(organs);
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
--- Nutrition Project/spc/default_ORGLANG.sql
+++ Nutrition Project/spc/default_ORGLANG.sql
@@ -13,13 +13,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Brain', N'Brain', '';
+SELECT '1', @ORNCD, 'Brain', N'Brain', 'brain.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'腦', N'腦', '';
+SELECT '2', @ORNCD, N'腦', N'腦', 'brain.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'脑', N'脑', '';
+SELECT '3', @ORNCD, N'脑', N'脑', 'brain.jpg';
 
 /* **********************************************************/
 
@@ -31,13 +31,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Eye', N'Eye', '';
+SELECT '1', @ORNCD, 'Eye', N'Eye', 'eye.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'眼睛', N'眼睛', '';
+SELECT '2', @ORNCD, N'眼睛', N'眼睛', 'eye.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'眼睛', N'眼睛', '';
+SELECT '3', @ORNCD, N'眼睛', N'眼睛', 'eye.jpg';
 
 /* **********************************************************/
 
@@ -49,13 +49,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Ear', N'Ear', '';
+SELECT '1', @ORNCD, 'Ear', N'Ear', 'ear.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'耳朵', N'耳朵', '';
+SELECT '2', @ORNCD, N'耳朵', N'耳朵', 'ear.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'耳朵', N'耳朵', '';
+SELECT '3', @ORNCD, N'耳朵', N'耳朵', 'ear.jpg';
 
 /* **********************************************************/
 
@@ -67,13 +67,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Nose', N'Nose', '';
+SELECT '1', @ORNCD, 'Nose', N'Nose', 'nose.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'鼻子', N'鼻子', '';
+SELECT '2', @ORNCD, N'鼻子', N'鼻子', 'nose.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'鼻子', N'鼻子', '';
+SELECT '3', @ORNCD, N'鼻子', N'鼻子', 'nose.jpg';
 
 /* **********************************************************/
 
@@ -85,13 +85,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Heart', N'Heart', '';
+SELECT '1', @ORNCD, 'Heart', N'Heart', 'heart.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'心臟', N'耳朵', '';
+SELECT '2', @ORNCD, N'心臟', N'心臟', 'heart.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'心脏', N'心脏', '';
+SELECT '3', @ORNCD, N'心脏', N'心脏', 'heart.jpg';
 
 /* **********************************************************/
 
@@ -103,13 +103,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Lung', N'Lung', '';
+SELECT '1', @ORNCD, 'Lung', N'Lung', 'lung.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'肺', N'肺', '';
+SELECT '2', @ORNCD, N'肺', N'肺', 'lung.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'肺', N'肺', '';
+SELECT '3', @ORNCD, N'肺', N'肺', 'lung.jpg';
    
 /* **********************************************************/
 
@@ -121,13 +121,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Kidney', N'Kidney', '';
+SELECT '1', @ORNCD, 'Kidney', N'Kidney', 'kidney.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'腎臟', N'腎臟', '';
+SELECT '2', @ORNCD, N'腎臟', N'腎臟', 'kidney.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'肾脏', N'肾脏', '';
+SELECT '3', @ORNCD, N'肾脏', N'肾脏', 'kidney.jpg';
 
 /* **********************************************************/
 
@@ -139,13 +139,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Stomach', N'Stomach', '';
+SELECT '1', @ORNCD, 'Stomach', N'Stomach', 'stomach.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'胃', N'胃', '';
+SELECT '2', @ORNCD, N'胃', N'胃', 'stomach.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'胃', N'胃', '';
+SELECT '3', @ORNCD, N'胃', N'胃', 'stomach.jpg';
   
 /* **********************************************************/
 
@@ -157,13 +157,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Small intestine', N'Small intestine', '';
+SELECT '1', @ORNCD, 'Small intestine', N'Small intestine', 'S-Intestine.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'小腸', N'小腸', '';
+SELECT '2', @ORNCD, N'小腸', N'小腸', 'S-Intestine.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'小肠', N'小肠', '';	
+SELECT '3', @ORNCD, N'小肠', N'小肠', 'S_Intestine.jpg';	
 
 /* **********************************************************/
 
@@ -175,13 +175,13 @@ INSERT INTO ORNMAS (ORNCD, ORNSEQ)
 SELECT @ORNCD, @SEQ;
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '1', @ORNCD, 'Large intestine', N'Large intestine', '';
+SELECT '1', @ORNCD, 'Large intestine', N'Large intestine', 'L-Intestine.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '2', @ORNCD, N'大腸', N'大腸', '';
+SELECT '2', @ORNCD, N'大腸', N'大腸', 'L-Intestine.jpg';
 
 INSERT INTO ORNLANG (ORNLANG, ORNCD, ORNNAM, ORNSPEC, ORNIMG)
-SELECT '3', @ORNCD, N'大肠', N'大肠', '';	
+SELECT '3', @ORNCD, N'大肠', N'大肠', 'L-Intestine.jpg';	
 
 /* **********************************************************/
 
